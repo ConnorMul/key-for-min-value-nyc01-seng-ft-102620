f6ed82e085cbdaae9fc7bbd015d61d28d21e9e08
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-29 22:12:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   
   name_hash.each do |name, num|
-    if lowest_key = nil || lowest_value 
+    if name == nil || lowest_value <
       lowest_key = name
       lowest_value = num
     end 
