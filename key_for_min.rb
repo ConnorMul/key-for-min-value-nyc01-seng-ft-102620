9569d791c8f4a8d64da6e7312979071932540689
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-29 21:34:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_value = 100
+  name_hash.each do |name, num|
+    if smallest_value > num 
+      smallest_value = name
+    end 
+  end 
+  smallest_value
 end
