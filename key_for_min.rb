ede6c720f2afc570ff50272453a3090e30350e21
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-29 21:50:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, num|
     if smallest_value > num
-      smallest_value = num
+      smallest_value = :name, num
     end 
   end 
   smallest_value
